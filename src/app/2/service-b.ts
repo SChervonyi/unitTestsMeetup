70469d2b6e895de1381aa41df50ca71ce6c9e1da
Serhii Chervonyi <chervoniys@gmail.com>
2018-09-23 18:54:06 +0300

    update

--- src/app/2/service-b.ts
+++ src/app/2/service-b.ts
@@ -1,6 +1,15 @@
-class UserService {
+import { Observable } from "rxjs";
 
-    public method(): string{
+export class ServiceB {
+
+    public property: any = { hello: 'Hello' };
+    public promiseProperty: Promise<number>;
+    public observableProperty = Observable.create(function(observer) {
+        observer.next('Hello');
+        observer.next('World');
+      });
+
+    public method(): string {
         return Math.random().toString(36).substring(7);
     }
 
@@ -10,6 +19,12 @@ class UserService {
                 resolve(true);
             }, 1000);
         });
+    }
+    public observableMethod(): Observable<string> {
+        return Observable.create(function(observer) {
+            observer.next('Hello');
+            observer.next('World');
+          });
     }
 
 }
