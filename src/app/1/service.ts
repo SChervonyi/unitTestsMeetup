6b91b53b179e1ad6f164c6b5ef59692bacbe4c9d
Serhii Chervonyi <chervoniys@gmail.com>
2018-09-29 22:25:25 +0300

    update

---
+++ src/app/1/service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Service {
+
+    public addFive(a: number): number {
+        return a + 5;
+    }
+
+    public greet(name: string): string {
+        if (!name) {
+            return '';
+        }
+
+        return `Hello ${name}`;
+    }
+}
