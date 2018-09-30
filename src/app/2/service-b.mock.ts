700d0ac87cb3681d5329d80a7b9fb5802e8177ec
Serhii Chervonyi <serhii.chervonyi@dev-pro.net>
2018-09-30 18:03:14 +0300

    Update

---
+++ src/app/2/service-b.mock.ts
@@ -0,0 +1,12 @@
+
+import { Observable, empty } from 'rxjs';
+
+export class ServiceBMock {
+  public property: any;
+  public promiseProperty = Promise.resolve();
+  public observableProperty = empty();
+
+  public method = () => { };
+  public promiseMethod = () => Promise.resolve();
+  public observableMethod = () => empty();
+}
